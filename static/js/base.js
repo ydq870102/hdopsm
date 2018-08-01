ce610d8da802ff28d6155f42769a01e957565dfb
ydq870102@163.com <ydq870102@163.com>
2018-08-01 17:52:49 +0800

    333

--- static/js/base.js
+++ static/js/base.js
@@ -26,3 +26,23 @@ $(function () {
     });
 });
 
+$('tbody tr').click(function (event) {
+    if(  event.target.name !='checked') {
+        $('.slidebar-wrapper').removeClass('hidden')
+    }
+});
+
+$('.slidebar-wrapper').click(function (event) {
+    if (event.target.className == 'slidebar-wrapper'){
+        $('.slidebar-wrapper').addClass('hidden')
+    }
+});
+
+$('.bk-tab2-head ul li').click(function () {
+    $('.tab2-nav-item').removeClass('actived')
+    $(this).addClass('actived')
+    var num =$(this).index()
+    $(".bk-tab2-content section").addClass('bk-tab2-pane').removeClass('active');
+    $(".bk-tab2-content section").eq(num).removeClass('bk-tab2-pane').addClass('active');
+});
+
